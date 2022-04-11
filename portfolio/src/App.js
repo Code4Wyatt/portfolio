e18015144113c236ec0f93cc243bdbd6261f3209
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-04-11 11:59:45 +0100

    Added Home Page and Styling
    Building Animations next

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,17 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import Layout from "../src/components/Layout/index.js";
-
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from '../src/components/Layout/index.js'
+import Home from '../src/components/Home/index.js'
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
-    </Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App

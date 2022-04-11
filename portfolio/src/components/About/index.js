8a28111066799115fcb123d338c32595215d3ebd
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-04-11 20:55:53 +0100

    Final Update
    Project completed

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -22,14 +22,15 @@ const About = () => {
    setTimeout(() => {
       setLetterClass('text-animate-hover')
       
-    }, 3000)
+    }, 4000)
   }, [])
   return (
     <>
     <div className="container about-page">
       <div className="text-zone">
         <h1>
-          <AnimatedLetters
+            <AnimatedLetters
+               letterClass={letterClass}
             strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
             idx={15}
           />
